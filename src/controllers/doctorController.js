871bc491e5448ee33f58d6db5e33cbf8753be651
Duck2Nguyen <taikhoanemail109@gmail.com>
2021-12-11 21:52:55 +0700

    het video 63

---
+++ src/controllers/doctorController.js
@@ -0,0 +1,21 @@
+import doctorService from "../services/doctorService"
+
+let getTopDocTorHome = async (req, res) => {
+    let limit = req.query.limit;
+    if (!limit) {
+        limit = 10;
+    }
+    try {
+        let response = await doctorService.getTopDoctorHome(+limit);
+        return res.status(200).json(response)
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            message: 'Error from sever'
+        })
+    }
+}
+module.exports = {
+    getTopDocTorHome: getTopDocTorHome
+}
