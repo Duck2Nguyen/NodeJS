560d83932eb9f32e329a8ef323f17e927d0aecbc
Duck2Nguyen <taikhoanemail109@gmail.com>
2021-12-24 21:33:48 +0700

    het video 81

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -82,11 +82,25 @@ let getScheduleByDate = async (req, res) => {
     }
 }
 
+let getExtraInforDoctorById = async (req, res) => {
+    try {
+        let infor = await doctorService.getExtraInforDoctorById(req.query.doctorId);
+        return res.status(200).json(infor)
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            message: 'Error from sever'
+        })
+    }
+}
+
 module.exports = {
     getTopDocTorHome: getTopDocTorHome,
     getAllDoctors: getAllDoctors,
     postInfoDoctor: postInfoDoctor,
     getDetailDoctorById: getDetailDoctorById,
     bulkCreateSchedule: bulkCreateSchedule,
-    getScheduleByDate: getScheduleByDate
+    getScheduleByDate: getScheduleByDate,
+    getExtraInforDoctorById: getExtraInforDoctorById
 }
