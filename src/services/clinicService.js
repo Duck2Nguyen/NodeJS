3141523722b7fea3cdc9e881dba929bd4d88c28c
Duck2Nguyen <taikhoanemail109@gmail.com>
2022-01-01 22:23:22 +0700

    het video 96

---
+++ src/services/clinicService.js
@@ -0,0 +1,37 @@
+import bodyParser from "body-parser"
+import db from "../models/index"
+
+
+let createClinic = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.name || !data.address || !data.imageBase64 || !data.descriptionHTML || !data.descriptionMarkdown) {
+                resolve({
+                    errCode: 1,
+                    errMessage: "Missing parameter"
+                })
+            } else {
+                await db.Clinic.create({
+                    name: data.name,
+                    image: data.imageBase64,
+                    address: data.address,
+                    descriptionHTML: data.descriptionHTML,
+                    descriptionMarkdown: data.descriptionMarkdown
+                })
+                resolve({
+                    errCode: 0,
+                    errMessage: 'Ok'
+                })
+
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+
+
+module.exports = {
+    createClinic: createClinic,
+}
